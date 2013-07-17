c80ad137554cbfdae8e77567f576fb73f07901eb
jquag <john.quagliata@gmail.com>
2013-07-17 08:32:19 -0500

    checkpoint

--- defend_the_castle.rb
+++ defend_the_castle.rb
@@ -5,13 +5,20 @@ module IRBChallenge
 
     def initialize(game)
       @game = game
-      @complete = true #TODO change this back to false
+      @complete = false
+      @dragon = Dragon.new
     end
 
     def help_message
       ['Defend the castle from the dragon.', '',
-       'Employ strategy:',
-       '> g.o[bjective].employ {|dragon_maneuver| <strategic response>}']
+       'Setup your defenses (the defense must respond to all dragon attacks):',
+       '> g.defend_with(defense)', '',
+       "Thwart the dragon's attack (the dragon will be passed to the defense method when attacking):",
+       '> dragon.thwart', '',
+       "If the dragon's state is :weakened, then fire the catapult:",
+       '> dragon.state',
+       '> g.fire_catapult'
+      ]
     end
 
     def to_s
@@ -24,6 +31,34 @@ module IRBChallenge
 
     def instance_variable_set(_, _)
       IRBChallenge.message('Sorry, that call is not allowed here.')
+    end
+
+    def fire_catapult
+      if @dragon.state == :weakened
+        IRBChallenge.message('Direct hit! The dragon was too weak to dodge the projectile.', 'Congratulations, you have successfully defended the castle.')
+        @complete = true
+        @game.level_complete(self)
+      else
+        IRBChallenge.message('Missed! The dragon had too much energy and easily dodged the projectile.')
+      end
+    end
+
+    private
+
+    class Dragon
+      attr_reader :state
+
+      def initialize
+        @state = :fierce
+      end
+
+      def thwart
+        @thwarted_count += 1
+      end
+
+      private
+
+      attr_accessor :attacking
     end
   end
 end
